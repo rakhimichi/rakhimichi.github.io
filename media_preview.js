04f5862bce0649c30777ac0a9603d58e356d1d1b
rakhimichi <kirill.rakhimov@myy.haaga-helia.fi>
2025-05-05 14:35:27 +0300

    🎯 3rd Final version for submission – all criteria met

--- media_preview.js
+++ media_preview.js
@@ -1,12 +1,15 @@
-let previewBox = document.getElementById("image-preview");
-let galleryImages = document.querySelectorAll(".gallery-grid img");
-
-galleryImages.forEach(function(image) {
-  image.addEventListener("mouseenter", function() {
-    previewBox.innerHTML = "<img src='" + image.src + "' alt='" + image.alt + "'>";
-    previewBox.style.display = "block";
-  });
-  image.addEventListener("mouseleave", function() {
-    previewBox.style.display = "none";
-  });
-});
+if (window.innerWidth > 768) {
+    let previewBox = document.getElementById("image-preview");
+    let galleryImages = document.querySelectorAll(".gallery-grid img");
+  
+    galleryImages.forEach(function(image) {
+      image.addEventListener("mouseenter", function() {
+        previewBox.innerHTML = "<img src='" + image.src + "' alt='" + image.alt + "'>";
+        previewBox.style.display = "block";
+      });
+  
+      image.addEventListener("mouseleave", function() {
+        previewBox.style.display = "none";
+      });
+    });
+  }
